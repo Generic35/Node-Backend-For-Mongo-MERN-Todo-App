e98e00e26ecfef72fe7555234415185ac34a04e3
Eric L <ricklamontagne@gmail.com>
2023-06-19 20:16:00 +0300

    (feat) Change request to get

--- server.js
+++ server.js
@@ -37,10 +37,12 @@ app.delete('/todo/delete/:id', async (req, res) => {
   res.json(result)
 })
 
-app.put('/todo/complete/:id', async (req, res) => {
+app.get('/todo/complete/:id', async (req, res) => {
   const todo = await Todo.findById(req.params.id, { complete: true });
   todo.complete = !todo.complete;
   todo.save();
   res.json(todo)
 })
+
+
 app.listen(3001, () => { console.log(">>> Server started on port 3001") })
